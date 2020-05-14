5e5940305e582ee88edb8550fcf90c40f7446f23
knoppixmeister <knoppixmeister@gmail.com>
2020-05-14 21:11:16 +0300

    commit

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -4,6 +4,6 @@
 public class Main {
 	public static void main(String[] args) {
 		System.out.println("1111");
-		System.out.println("1111__111111");
+		System.out.println(System.getProperty("java.version"));
 	}
 }
